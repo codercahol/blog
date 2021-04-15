ed423a0bb9042e415758d72755d696530a818d85
LP <5104722+codercahol@users.noreply.github.com>
2021-04-15 13:23:43 -0700

    Built `first post`

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "Hello, World",
-        "excerpt":"More to come in the next weeks as I sit at home reading during the ongoing public health crisis.   ","categories": ["blog"],
-        "tags": ["update"],
-        "url": "/blog/intro/",
+        "title": "A Vignette of Scientific Other",
+        "excerpt":"Our experienced reality tends to color outside of the lines set by science. As soon as you step outside, shoes crunching on gravel, you are surrounded by the scientific Other, phenomena that are described with terms like non-linear and non-equilibrium. The gravel we step on and the rubber of our...","categories": ["blog"],
+        "tags": ["science"],
+        "url": "/blog/other/",
         "teaser": null
       }]
